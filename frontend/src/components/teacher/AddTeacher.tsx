3f599290bb56ac9f7445e3020fc1e2a83c19d8af
marce <marcell.dechant@neuefische.de>
2023-07-21 23:49:52 +0200

    Create add teacher component & Create teachers table

---
+++ frontend/src/components/teacher/AddTeacher.tsx
@@ -0,0 +1,71 @@
+import {TeacherRequest} from "../../models/TeacherRequest.ts";
+import Box from "@mui/material/Box";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import {ChangeEvent, FormEvent, useState} from "react";
+import {TextField} from "@mui/material";
+import Button from "@mui/material/Button";
+import Swal from "sweetalert2";
+
+type AddTeacherProps = {
+    addTeacher: (teacher: TeacherRequest) => Promise<void>;
+}
+
+export default function AddTeacher(props: AddTeacherProps) {
+
+    const addTeacher = props.addTeacher;
+    const [teacher, setTeacher] = useState<TeacherRequest>({firstName: "", lastName: "", salutation: ""});
+
+    function handleChangeSalutation(event: SelectChangeEvent) {
+        const value = event.target.value;
+        setTeacher({...teacher, salutation: value});
+    }
+
+    function handleChangeName(event: ChangeEvent<HTMLInputElement>) {
+        setTeacher({...teacher, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmitNewTeacher(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Hinzufügen...',
+            text: 'Bitte warten...',
+            didOpen() {
+                Swal.showLoading()
+                addTeacher(teacher).then(() => {
+                    Swal.fire({
+                        title: 'Hinzugefügt!',
+                        text: teacher.firstName + ' ' + teacher.lastName + ' wurde hinzugefügt.',
+                        icon: 'success',
+                    }).then();
+                });
+            }
+        });
+    }
+
+    return (
+        <Box sx={{minWidth: 120, mt: 2}} component={"form"} onSubmit={handleSubmitNewTeacher}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Anrede</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={teacher.salutation}
+                    label="Anrede"
+                    onChange={handleChangeSalutation}
+                >
+                    <MenuItem value={"Frau"}>Frau</MenuItem>
+                    <MenuItem value={"Herr"}>Herr</MenuItem>
+                    <MenuItem value={"Divers"}>Divers</MenuItem>
+                </Select>
+                <TextField id="outlined-basic" name="firstName" onChange={handleChangeName} label="Vorname"
+                           variant="outlined"/>
+                <TextField id="outlined-basic" name="lastName" onChange={handleChangeName} label="Nachname"
+                           variant="outlined"/>
+                <Button type={"submit"}>Hinzufügen</Button>
+            </FormControl>
+        </Box>
+    );
+}
---
+++ frontend/src/components/teacher/TeacherTable.tsx
@@ -0,0 +1,34 @@
+import {Teacher} from "../../models/Teacher.ts";
+import TeacherRow from "./TeacherRow.tsx";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import Paper from "@mui/material/Paper";
+
+type TeacherTableProps = {
+    teachers: Teacher[];
+    deleteTeacher: (id: string) => Promise<void>;
+}
+
+export default function TeacherTable(props: TeacherTableProps) {
+
+    const teachers: Teacher[] = props.teachers;
+    const teacherRows = teachers.map((teacher) => <TeacherRow key={teacher.id} deleteTeacher={props.deleteTeacher}
+                                                              teacher={teacher}/>)
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell component={"th"}><strong>Anrede</strong></TableCell>
+                        <TableCell component={"th"}><strong>Vorname</strong></TableCell>
+                        <TableCell component={"th"}><strong>Nachname</strong></TableCell>
+                        <TableCell component={"th"}><strong>Löschen</strong></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {teacherRows}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
